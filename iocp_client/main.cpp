f4596d9f4cff70c8648ac49b1dbfdd1543e7c1fb
merry5596 <merry5596@naver.com>
2024-03-13 13:00:24 +0900

    feature: 공백포함 메시지 송신 가능

--- iocp_client/main.cpp
+++ iocp_client/main.cpp
@@ -6,22 +6,22 @@ const char* SERVER_IP = "127.0.0.1";
 bool isRecvRun;
 
 int main(void) {
-	cout << "접속하시겠습니까? (y/n)" << endl;
+	cout << "접속 하시겠습니까? (y/n)" << endl;
 	char a;
 	cin >> a;
 	if (a == 'n') {
 		return 0;
 	}
+	cin.ignore();
 
 	ClientNetwork network;
 	network.Init(SERVER_PORT, SERVER_IP);
 	network.Start();	//Recv 시작
 
 	string msg;
+	cout << "메시지를 입력하세요(종료는 exit): ";
 	while (1) {
-		cout << "메시지를 입력하세요(종료는 exit): ";
-		//getline(cin, msg);
-		cin >> msg;
+		getline(cin, msg);
 		if (msg == "exit") {
 			isRecvRun = false;
 			break;
